6042bbdf69bc6a13fd74e9a34ba8651482e9fa20
Oleksandr Khotemskyi <xotabu4@gmail.com>
2019-05-14 16:32:42 +0300

    added local headless chromium

--- e2e/wdio.config.js
+++ e2e/wdio.config.js
@@ -1,30 +1,44 @@
+const chromium = require("chromium");
+const chromedriver = require("chromedriver");
+
+// Making BaseURL available everywhere
+global.SUT_URL = process.env.SUT_URL || "http://todomvc.com/examples/vue/";
+
 exports.config = {
     specs: ["./tests/*.js"],
     sync: true,
-    // services: ["selenium-standalone"],
-    // port: 4844,
-    //port: 4444, 
-    hostname: "temporary-chrome",
-    //path: '/',
-    maxInstances: 1,
-    maxInstancesPerCapability: 1, 
+    //services: ["selenium-standalone"],
+    port: 9515, // default for ChromeDriver
+    path: "/",
+    // ...
+    // services: ["chromedriver"],
+    // options
+    //chromeDriverArgs: ['--port=9515', '--url-base=\'/\''], // default for ChromeDriver
+    //chromeDriverLogs: './',
+    maxInstancesPerCapability: 1,
     capabilities: [
         {
             browserName: "chrome",
+            "goog:chromeOptions": {
+                args: ["--headless"],
+                binary: chromium.path
+            },
             maxInstances: 1
         }
     ],
     reporters: ["spec"],
-    baseUrl: "http://todo-app:8080",
+    baseUrl: global.SUT_URL,
     framework: "mocha",
     logLevel: "silent",
     mochaOpts: {
         ui: "bdd",
         timeout: 120000 // in ms
     },
-
-    before: function () {
-        browser.setWindowSize(1280, 1024)
+    onPrepare: function() {
+        chromedriver.start();
+    },
+    before: function() {
+        browser.setWindowSize(1280, 1024);
     },
     /**
      * Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) ends.
@@ -32,9 +46,14 @@ exports.config = {
      */
     afterTest: function(test) {
         // faster than reload browser
-        browser.execute('window.localStorage.clear(); window.sessionStorage.clear()')
-        browser.refresh()
+        browser.execute(
+            "window.localStorage.clear(); window.sessionStorage.clear()"
+        );
+        browser.refresh();
 
         // browser.reloadSession();
+    },
+    onComplete: function() {
+        chromedriver.stop();
     }
 };
